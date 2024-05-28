0a88d2767aedda8e1f04c6e237061e6164310f7d
unknown <yashkoli836@gmail.com>
2024-05-28 23:40:19 +0530

    added styled buttons

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css';
-const brand = () => {
-  return (
-    <div>brand</div>
-  )
-}
 
-export default brand
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt='g'/>
+    </div>
+    <div>
+      <img src={slack} alt='s'/>
+    </div>
+    <div>
+      <img src={atlassian} alt='a'/>
+    </div>
+    <div>
+      <img src={dropbox} alt='d'/>
+    </div>
+    <div>
+      <img src={shopify} alt='s'/>
+    </div>
+  </div>
+);
+
+export default Brand
--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './WhatGPT3.css';
+import{Feature} from '../../components'
+
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+          The Possibilities are beyong your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
+    </div>
   )
 }
 
