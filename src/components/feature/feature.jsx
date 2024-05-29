75824e2de2e5de906ab3f1068d1b59dcc9258748
unknown <yashkoli836@gmail.com>
2024-05-29 20:14:28 +0530

    some new changes

--- src/components/feature/feature.jsx
+++ src/components/feature/feature.jsx
@@ -9,7 +9,7 @@ const feature = ({title, text}) => {
         <h1>{title}</h1>
       </div>
       <div className='gpt3__features-container_feature-text'>
-        {text}
+        <p>{text}</p>
       </div>
     </div>
   )
