0c09b2285b61b291da919e37f5cbb73b713d0450
yashkoli836@gmail.com <yashkoli836@gmail.com>
2024-05-31 14:50:19 +0530

    blog addition

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './article.css';
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt="blog img" />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <p>{title}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
