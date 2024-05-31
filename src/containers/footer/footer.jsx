f8d8576a9c7ea83b5851d5811815b85792024f3d
yashkoli836@gmail.com <yashkoli836@gmail.com>
2024-05-31 20:28:43 +0530

    website done

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>
+          Do You want to step in to the future before others
+        </h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt="logo" />
+          <p>XYZ K12 182 DK Abcde, All Rights Reserved</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>XYZ K12 182 DK Abcde</p>
+          <p>123456789</p>
+          <p>companymail@example.com</p>
+        </div>
+      </div>
+
+      <div className='gpt3__footer-copyright'>
+        <p>2024 GPT-3. All rights reserved</p>
+      </div>
+      
+    </div>
   )
 }
 
